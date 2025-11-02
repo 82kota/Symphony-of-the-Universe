a90ed82a916abd37b7db29b02a16c641595edcd5
Vasilii <zubarevprimary@gmail.com>
2025-11-02 06:04:56 +0000

    add audio player bar bottom listen page

--- app/listen/page.tsx
+++ app/listen/page.tsx
@@ -11,7 +11,7 @@ export type SoundTrackHandle = {
 export default function ListenPage() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   return (
-    <div className="h-full flex flex-col items-center justify-center py-30 gap-5 bg-black">
+    <div className="h-full flex flex-col items-center justify-center pt-30 gap-5 bg-black">
       {/*Todo: refactor*/}
       <div className="flex w-full items-center justify-center gap-4 px-20">
         <p className="text-white text-2xl">Moon meteorites</p>
@@ -37,13 +37,15 @@ export default function ListenPage() {
           colour="blue"
         />
       </div>
-      <div className=" transition-all duration-200 border-white hover:border-purple-400 text-white hover:text-purple-400">
-        <button
-          className=" p-4 rounded-full border  cursor-pointer "
-          onClick={() => audioRef.current?.play()}
-        >
-          <Play className=" hover:font-bold"></Play>
-        </button>
+      <div className="sticky bottom-0 w-full border-t py-2 bg-gray-950 ">
+        <div className="flex items-center justify-center">
+          <button
+            className=" p-4 rounded-full border transition-all border-white hover:border-purple-400 text-white hover:text-purple-400 duration-200 cursor-pointer "
+            onClick={() => audioRef.current?.play()}
+          >
+            <Play className=" hover:font-bold"></Play>
+          </button>
+        </div>
       </div>
     </div>
   );
