6beaf84e7ee00a85d8d03a1d715b3afe4ac90369
Vasilii <zubarevprimary@gmail.com>
2025-11-02 01:09:27 +0000

    hide sarah pic

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,11 +5,13 @@ export default function AboutPage() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-black mt-10 gap-5">
       <p className="text-4xl text-white">About</p>
+      {/*
       <Image
         src={sarah_img}
         alt="Sarah image"
         className="w-xl rounded-3xl"
       ></Image>
+      */}
     </div>
   );
 }
